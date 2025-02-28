95d01f641a145ffd831918dd18464e00202c778c
neilSoftWorks <neilbay@gmail.com>
2025-03-01 10:42:16 +1300

    add tests

--- src/main/java/com/example/service/ApplicationService.java
+++ src/main/java/com/example/service/ApplicationService.java
@@ -40,4 +40,8 @@ public class ApplicationService {
                 })
                 .orElse(null);
     }
+
+    public void deleteBusinessDetails(Long id) {
+        businessDetailsRepository.deleteById(id);
+    }
 }
